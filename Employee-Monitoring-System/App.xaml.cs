b56d575b55ea541aa3c8ebd4c732be46568d57c5
Karan Gondaliya <139796588+karangondaliya@users.noreply.github.com>
2024-12-17 18:36:57 +0530

    Initial Commit

---
+++ Employee-Monitoring-System/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Employee_Monitoring_System
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Employee-Monitoring-System/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee_Monitoring_System
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
